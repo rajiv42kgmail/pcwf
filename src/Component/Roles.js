8ee2ee5b2ac277cd319c2266594633a14a2a72bd
rajeev prasad42k <rajiv.prasad42k@gmail.com>
2023-05-07 02:06:55 +0530

    Working roles management changes

---
+++ src/Component/Roles.js
@@ -0,0 +1,104 @@
+import React from 'react';
+import { useState } from 'react';
+const Roles = () => {
+    const handleChange = (e) => {
+        e.preventDefault(); 
+        if(e.target.name == 'roleType')  {   
+            let selected='';//will be selected option in select
+            let selected_opt=(e.target.selectedOptions);
+            console.log(selected_opt)
+            for (let i = 0; i < selected_opt.length; i++){
+                selected += (selected_opt.item(i).value) + ",";
+            }
+            selected = selected.substring(0,selected.length-1);
+            setUser(prev => ({...prev,[e.target.name]:selected }))
+
+        }   
+
+
+
+       // console.log(user)
+    }
+
+   const[user,setUser] = useState({
+        roleType:"",
+        roles:"",
+        rolesuser:""
+    }) 
+    const[final,setFinal] = useState({})
+    const printData = (e) => {
+        e.preventDefault();
+        setFinal(user);
+//        console.log(final.roleType)
+    }
+
+
+
+    return (
+        <>
+                    <h1>{final.roleType}</h1>
+
+            <div className="container-fluid px-4">
+                <h1 className="mt-4">Roles Management</h1>
+                <div className="row">
+                    <form onSubmit={printData}>
+                        <label htmlFor='RolesType'>Role Type</label>
+                        <select className="form-select" multiple aria-label="multiple select example"  name="roleType"  onChange={handleChange.bind(this)}>
+  <option value="PCU">PCU</option>
+  <option value="BA">BA</option>
+  <option value="VcodeUser">Veracode User</option>
+</select>
+<br />
+<label htmlFor='Roles'>Roles</label>
+<div className="form-check">
+  <input className="form-check-input" type="checkbox" name="roles" value="createPages" id="createPage"  onChange={handleChange}/>
+  <label className="form-check-label" for="createPage">
+    Create Pages
+  </label>
+  </div>
+  <div className="form-check">
+  <input className="form-check-input" type="checkbox" name="roles" value="editPages" id="editPage" onChange={handleChange} />
+  <label className="form-check-label" for="editPage">
+    Edit Pages
+  </label>
+  </div>
+  <div className="form-check">
+
+  <input className="form-check-input" type="checkbox" name="roles" value="deletePages" id="delPage" onChange={handleChange} />
+  <label className="form-check-label" for="delPage">
+    Delete Pages
+  </label>
+</div>
+<br />
+<br />
+<label htmlFor='RolesUser'>Roles User</label>
+<div className="form-check">
+  <input className="form-check-input" type="radio" name="rolesuser" id="VeracodeUser" value="vcodeuser" onChange={handleChange}/>
+  <label className="form-check-label" for="VeracodeUser">
+    Veracode User
+  </label>
+</div>
+<div className="form-check">
+  <input className="form-check-input" type="radio" name="rolesuser" id="admin" value="admin" onChange={handleChange} checked />
+  <label className="form-check-label" for="admin">
+    Admin
+  </label>
+</div>
+
+<br />
+<br />
+
+<button>Submit</button>
+
+
+                    </form>
+                </div>
+            </div>
+
+        </>
+
+
+    );
+
+}
+export default Roles;
