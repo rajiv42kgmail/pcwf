9d8178cc1424b00262593afcc05a6d348e7f5339
rajeev prasad42k <rajiv.prasad42k@gmail.com>
2023-05-08 23:42:02 +0530

    Displayed submitted info in roles page

--- src/Component/Roles.js
+++ src/Component/Roles.js
@@ -13,19 +13,6 @@ const Roles = () => {
             selected = selected.substring(0,selected.length-1);
             setUser(prev => ({...prev,[e.target.name]:selected }))
         }   
-
-        if(e.target.name == 'roles')  { 
-          let checkedValue = '';
-          var checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
-          for (let i = 0; i < checkboxes.length; i++){
-               checkedValue += checkboxes[i].value + ",";
-          } 
-         // console.log(checkedValue)
-         checkedValue = checkedValue.substring(0,checkedValue.length-1);
-          setUser(prev => ({...prev,[e.target.name]:checkedValue }))
-      }   
-
-
         //console.log(user)
     }
 
@@ -37,6 +24,23 @@ const Roles = () => {
     const[final,setFinal] = useState({})
     const printData = (e) => {
         e.preventDefault();
+        let checkedValue = '';
+        var checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+        for (let i = 0; i < checkboxes.length; i++){
+             checkedValue += checkboxes[i].value + ",";
+        } 
+       // console.log(checkedValue)
+       checkedValue = checkedValue.substring(0,checkedValue.length-1);
+        user.roles=checkedValue
+        let checkedRadioValue = '';
+        var radioButtons = document.querySelectorAll('input[type="radio"]:checked');
+        for (let i = 0; i < radioButtons.length; i++){
+          checkedRadioValue += radioButtons[i].value + ",";
+        } 
+       // console.log(checkedValue)
+       checkedRadioValue = checkedRadioValue.substring(0,checkedRadioValue.length-1);
+        user.rolesuser=checkedRadioValue
+           
         setFinal(user);
 //        console.log(final.roleType)
     }
@@ -45,6 +49,7 @@ const Roles = () => {
         <>
                     <h1>{final.roleType}</h1>
                     <h1>{final.roles}</h1>
+                    <h1>{final.rolesuser}</h1>
 
 
             <div className="container-fluid px-4">
@@ -60,20 +65,20 @@ const Roles = () => {
 <br />
 <label htmlFor='Roles'>Roles</label>
 <div className="form-check">
-  <input className="form-check-input" type="checkbox" name="roles" value="createPages" id="createPage"  onChange={handleChange}/>
+  <input className="form-check-input" type="checkbox" name="roles" value="createPages" id="createPage" />
   <label className="form-check-label" for="createPage">
     Create Pages
   </label>
   </div>
   <div className="form-check">
-  <input className="form-check-input" type="checkbox" name="roles" value="editPages" id="editPage" onChange={handleChange} />
+  <input className="form-check-input" type="checkbox" name="roles" value="editPages" id="editPage" />
   <label className="form-check-label" for="editPage">
     Edit Pages
   </label>
   </div>
   <div className="form-check">
 
-  <input className="form-check-input" type="checkbox" name="roles" value="deletePages" id="delPage" onChange={handleChange} />
+  <input className="form-check-input" type="checkbox" name="roles" value="deletePages" id="delPage"/>
   <label className="form-check-label" for="delPage">
     Delete Pages
   </label>
@@ -82,13 +87,13 @@ const Roles = () => {
 <br />
 <label htmlFor='RolesUser'>Roles User</label>
 <div className="form-check">
-  <input className="form-check-input" type="radio" name="rolesuser" id="VeracodeUser" value="vcodeuser" onChange={handleChange}/>
+  <input className="form-check-input" type="radio" name="rolesuser" id="VeracodeUser" value="vcodeuser" />
   <label className="form-check-label" for="VeracodeUser">
     Veracode User
   </label>
 </div>
 <div className="form-check">
-  <input className="form-check-input" type="radio" name="rolesuser" id="admin" value="admin" onChange={handleChange} checked />
+  <input className="form-check-input" type="radio" name="rolesuser" id="admin" value="admin" />
   <label className="form-check-label" for="admin">
     Admin
   </label>
