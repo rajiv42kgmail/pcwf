337a0ffbc31c243ced9fd396fe07d1d75a95f7db
rajeev prasad42k <rajiv.prasad42k@gmail.com>
2023-07-03 13:55:00 +0530

    Modified navigation

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -16,34 +16,38 @@ const Navbar = () => {
                             <NavLink to={"/form_type"} className="nav-link">                                        <div className="sb-nav-link-icon"><i className="fas fa-tachometer-alt"></i></div>
                                 Form Type
                             </NavLink>
-                            
-                            <a className="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
-                                <div className="sb-nav-link-icon"><i className="fas fa-book-open"></i></div>
-                                Pages
-                                <div className="sb-sidenav-collapse-arrow"><i className="fas fa-angle-down"></i></div>
-                            </a>
-                            <div className="collapse" id="collapsePages" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
-                                <nav className="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
 
-                                    <NavLink className="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseAuth" aria-expanded="false" aria-controls="pagesCollapseAuth">
-                                        System
-                                        <div className="sb-sidenav-collapse-arrow"><i className="fas fa-angle-down"></i></div>
-                                    </NavLink>
+                            
+                            <div id="collapsePages" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
+                                
+                                <nav className="sb-sidenav-menu-nested nav collapsed" id="sidenavAccordionPages">
+                                <a className="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseAuth" aria-expanded="false" aria-controls="pagesCollapseAuth">System
+                                    <div className="sb-sidenav-collapse-arrow"><i className="fas fa-angle-down"></i></div>
+                
+                                </a>
                                     <div className="collapse" id="pagesCollapseAuth" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordionPages">
                                         <nav className="sb-sidenav-menu-nested nav">
-                                             <NavLink to={"/userlist"} className="nav-link">User</NavLink>
+                                            <NavLink to={"/userlist"} className="nav-link">User</NavLink>
                                         </nav>
                                         <nav className="sb-sidenav-menu-nested nav">
                                             <NavLink to={"/roleslist"} className="nav-link">Roles</NavLink>
                                         </nav>
-                                      
-                                 
-                                    
+                                    </div>
 
-                                       
-                                      
-
-
+                                </nav>
+                            </div>
+                            <div id="collapsePages_3" aria-labelledby="headingThree" data-bs-parent="#sidenavAccordion">
+                                <nav className="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages_3">
+                                <a className="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseAuth_3" aria-expanded="false" aria-controls="pagesCollapseAuth_3">Admin Functions
+                                    <div className="sb-sidenav-collapse-arrow"><i className="fas fa-angle-down"></i></div>
+                                </a>
+                                    <div className="collapse" id="pagesCollapseAuth_3" aria-labelledby="headingThree" data-bs-parent="#sidenavAccordionPages_3">
+                                        <nav className="sb-sidenav-menu-nested nav">
+                                            <NavLink to={"/form_type_list"} className="nav-link">Form Type</NavLink>
+                                        </nav>
+                                        <nav className="sb-sidenav-menu-nested nav">
+                                            <NavLink to={"/account_list"} className="nav-link">Accounts</NavLink>
+                                        </nav>
                                     </div>
 
                                 </nav>
